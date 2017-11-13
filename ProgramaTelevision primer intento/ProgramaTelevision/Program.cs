a73049e7897473ec3b7e06b455dad5721e27cfd1
AtaiVKC <victorimperium@hotmail.com>
2017-11-13 20:50:07 +0100

    Primer intento del programa
    Primer intento del programa

---
+++ ProgramaTelevision primer intento/ProgramaTelevision/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaTelevision
+{
+    class Program
+    {
+        /*
+• Mostrar programación semanal: Al pulsar la opción,
+ se mostrará la programación de la 
+semana completa (lunes a viernes).  
+
+• Mostrar programación diaria: Al pulsar la opción, se pedirá el día de la semana que se quiere consultar. Seguidamente, se mostrará la programación del día elegido. 
+
+• Mostrar contenidos por día: Al pulsar la opción, se pulsará el día de la semana que se desea consultar. A continuación, se mostrará la cantidad de minutos que se destina ese día a cada uno de los contenidos del programa*/
+        static public void decirHoraIni(Semana semana)
+        {
+           // Console.WriteLine(semana.getSemana());
+        }
+
+        static public void menu(ref int seleccion)
+        {
+            Console.WriteLine("Programa para controlar y modificar emisiones de los programas de TV durante la semana.\n");
+            Console.WriteLine("Por favor, eliga una de las siguientes opciones:");
+            Console.WriteLine("1. Asignar un programa nuevo a un día y horario concreto\n" +
+                              "2. Eliminar un programa\n" +
+                              "3. Modificar los minutos de un programa\n" +
+                              "4. Mostrar programación semanal\n" +
+                              "5. Mostrar programación diaria\n" +
+                              "6. Mostrar contenidos por día\n" +
+                              "7. Salir\n");
+            Console.WriteLine("Selección: ");
+            seleccion = Int32.Parse(Console.ReadLine());
+        }
+
+        static public void resolver(ref Semana semana, int seleccion)
+        {
+            int mins = 0;
+
+            switch (seleccion)
+            {
+                case 1:
+                    //día, horario y datos del programa
+                    int dia, horario, minutos;
+                    string nombre, tipoPrograma;
+                    Console.WriteLine("Escribe el día que quieres guardar el programa (lunes=1, martes=2, etc.: ");
+                    dia = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("En qué horario quieres guardar el programa? (Primera hora=1, matinal=2 etc.:");
+                    horario = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Escribe el nombre del programa: ");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("Escribe el tipo de programa que es (informativo, serie, pelicula, etc.: ");
+                    tipoPrograma = Console.ReadLine();
+                    Console.WriteLine("Escribe los minutos que dura: ");
+                    minutos = Int32.Parse(Console.ReadLine());
+
+                    semana.addPrograma(dia, horario, minutos, nombre, tipoPrograma);
+                    break;
+                case 2:
+                    //pedirán los datos de día y horario, y se eliminarán los datos del programa para ese horario.
+                    Console.WriteLine("Escribe el día del que quieres borrar el programa (lunes=1, martes=2, etc.: ");
+                    dia = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("En qué horario quieres borrar el programa? (Primera hora=1, matinal=2 etc.:");
+                    horario = Int32.Parse(Console.ReadLine());
+
+                    semana.remPrograma(dia, horario);
+                    break;
+                case 3:
+                    /*se pedirán los datos de día y horario, y los minutos que se quiere aumentar o disminuir*/
+                    Console.WriteLine("Escribe el día del que quieres modificar el programa (lunes=1, martes=2, etc.: ");
+                    dia = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("En qué horario quieres modificar el programa? (Primera hora=1, matinal=2 etc.:");
+                    horario = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Cuántos minutos quieres aumentaro disminuir del programa? (escribe con signo negativo en caso de restar minutos: ");
+                    mins = Int32.Parse(Console.ReadLine());
+
+                    semana.modMinutos(dia, horario, mins);
+                    break;
+                case 4:
+                    
+                    semana.getSemana();
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int seleccion = 0;
+            Semana semana = new Semana();
+            //decirHoraIni(semana);
+            while (seleccion != 7)
+            {
+                menu(ref seleccion); //Muestra las opciones.
+                resolver(ref semana, seleccion); //Según elijas, resolverá la opción elegida.
+            }
+            //Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ProgramaTelevision primer intento/ProgramaTelevision/Semana.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaTelevision
+{
+    /*
+• 8:00 – 10:00: Primera hora
+• 10:00 – 14:00: Matinal
+• 14:00 – 16:00: Mediodía.
+• 16:00 – 20:00: Tarde
+• 20:00 – 24:00: Noche  
+         */
+    class Semana
+    {
+        Dia[] dia = new Dia[7];
+
+        public Semana()
+        {
+            for (int i = 0; i < 7; i++)
+                dia[i] = new Dia();
+            dia[0].darNombresSemana("Lunes");
+            dia[1].darNombresSemana("Martes");
+            dia[2].darNombresSemana("Miércoles");
+            dia[3].darNombresSemana("Jueves");
+            dia[4].darNombresSemana("Viernes");
+            dia[5].darNombresSemana("Sábado");
+            dia[6].darNombresSemana("Domingo");
+        }
+
+        // ----------------------------------------METODOS DE LA CLASE SEMANA ----------------------------------------
+
+        public void getSemana() //MOSTRAR SEMANA DE PROGRAMAS
+        {
+            //return lunes.primeraHora.horarioInicio;
+            Console.WriteLine("Esta es la programación de toda la semana: ");
+            for (int i = 0; i > dia.Length; i++)
+            {
+                Console.WriteLine(dia[i].getNombreDia());
+            }
+
+            //dia[0].accederHorario();
+        }
+
+        public void addPrograma(int dia, int horario, int minutos, string nombre, string tipoPrograma) //AÑADE PROGRAMA TV NUEVO
+        {
+            this.dia[dia].addPrograma(horario, minutos, nombre, tipoPrograma);
+        }
+
+        public void remPrograma(int dia, int horario) //BORRA UN PROGRAMA TV
+        {
+            this.dia[dia].remPrograma(horario);
+        }
+        public void modMinutos(int dia, int horario, int mins) //AÑADIR O QUITAR MINUTOS DE PROGRAMA TV
+        {
+            this.dia[dia].modMinutos(horario, mins);
+        }
+
+
+        class Dia
+        {
+            //Horario primeraHora, matinal, mediodia, tarde, noche = new Horario();
+            Horario[] horario = new Horario[5];
+            string nombreDia;
+            public Dia()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    horario[i] = new Horario();
+                }
+
+                horario[0].establecerHorarioIniFin(8, 10);
+                horario[1].establecerHorarioIniFin(10, 14);
+                horario[2].establecerHorarioIniFin(14, 16);
+                horario[3].establecerHorarioIniFin(16, 20);
+                horario[4].establecerHorarioIniFin(20, 22);
+            }
+            // ----------------------------------------METODOS DE LA CLASE DIA ----------------------------------------
+            public void darNombresSemana(string nombre)
+            {
+                nombreDia = nombre;
+            }
+            public string getNombreDia()
+            {
+                return nombreDia;
+            }
+            public void addPrograma(int horario, int minutos, string nombre, string tipoPrograma) //AÑADE PROGRAMA TV NUEVO
+            {
+                this.horario[horario].addPrograma(minutos, nombre, tipoPrograma);
+            }
+
+            public int accederHorario()
+            {
+                return 0;
+              //  return primeraHora.decirIni();
+            }
+
+            public void remPrograma(int horario) //BORRA UN PROGRAMA TV
+            {
+                this.horario[horario].remPrograma();
+            }
+            public void modMinutos(int horario, int mins) //AÑADIR O QUITAR MINUTOS DE PROGRAMA TV
+            {
+                this.horario[horario].modMinutos(mins);
+            }
+        }
+
+        class Horario
+        {
+            string nombre, tipoContenido;
+            int duracion, horarioInicio, horarioFin;
+
+            // ----------------------------------------METODOS DE LA CLASE HORARIO ----------------------------------------
+            public int decirIni()
+            {
+                return horarioInicio;
+            }
+            public void establecerHorarioIniFin(int ini, int fin)
+            {
+                horarioInicio = ini;
+                horarioFin = fin;
+            }
+            public void addPrograma(int minutos, string nombre, string tipoPrograma) //AÑADE PROGRAMA TV NUEVO
+            {
+                this.nombre = nombre;
+                duracion = minutos;
+                tipoContenido = tipoPrograma;
+            }
+            public void remPrograma() //BORRA UN PROGRAMA TV
+            {
+                nombre = "NADA";
+                duracion = 0;
+                tipoContenido = "NADA";
+            }
+            public void modMinutos(int mins) //AÑADIR O QUITAR MINUTOS DE PROGRAMA TV
+            {
+                duracion += mins;
+            }
+        }
+
+
+    }
+
+}
